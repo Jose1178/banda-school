17cc7852833f7b8100e23b4dacd017fa6433cb7d
Joseph <wamiti8711@gmail.com>
2023-03-27 07:02:32 -0400

    courseid model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_103953) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_110025) do
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_103953) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "course_id"
   end
 
 end
