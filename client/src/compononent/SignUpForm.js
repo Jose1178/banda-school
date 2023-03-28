20734e782244441198ca09fae69323838d2a63a0
+ <+>
2023-03-28 04:43:14 +0300

    signup-form

--- client/src/compononent/SignUpForm.js
+++ client/src/compononent/SignUpForm.js
@@ -1,32 +1,34 @@
-// function SignUpForm() {
+ function SignUpForm() {
 
-//     return (
-//       <div>
-//         <div className="flex justify-center">
-//         <form className="flex flex-col px-10 bg-slate-300 max-w-lg my-8">
-//                 <label>Name:</label>
-//                 <input type="text" className="label2" name="name"/>
+  return (
+      <div>
+        {/* <div className="flex justify-center"> */}
+        <form >
+        {/* className="flex flex-col px-10 bg-slate-300 max-w-lg my-8" */}
+                <label>Name:</label>
+                <input type="text" className="label2" name="name"/>
 
-//                 <label>Admission Number:</label>
-//                 <input type="text" className="label2" name="adminnumber"/>
+                <label>Admission Number:</label>
+                <input type="text" className="label2" name="adminnumber"/>
 
-//                 <label>Email:</label>
-//                 <input type="email" className="label2" name="email"/>
+                <label>Email:</label>
+                <input type="email" className="label2" name="email"/>
 
-//                 <label>ID Number:</label>
-//                 <input type="number" className="label2" name="idnumber"/>
+                <label>ID Number:</label>
+                <input type="number" className="label2" name="idnumber"/>
 
-//                 <label>Gender:</label>
-//                 <input type="text" className="label2" name="gender"/>
+                <label>Gender:</label>
+                <input type="text" className="label2" name="gender"/>
 
-//                 <label>Password:</label>
-//                 <input type="password" className="label2" name="password"/>
+                <label>Password:</label>
+                <input type="password" className="label2" name="password"/>
 
-//                 <button className="bg-blue-800 w-max px-2 my-2 mx-auto rounded-md">SignUp</button>
-//             </form>
-//         </div>
-//       </div>
-//     )
-//   }
+                <button >SignUp</button>
+                {/* className="bg-blue-800 w-max px-2 my-2 mx-auto rounded-md" */}
+            </form>
+        </div>
+    //   </div>
+    )
+  }
 
-//   export default  SignUpForm;
+  export default  SignUpForm;
