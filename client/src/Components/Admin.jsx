13ebfbc30f38ae4c539fd6a6862df99ca8e2b54c
josephwamiti <wamiti8711@gmail.com>
2023-03-30 12:55:22 +0300

    admin

--- client/src/Components/Admin.jsx
+++ client/src/Components/Admin.jsx
@@ -9,7 +9,7 @@ function Admin() {
           htmlFor="email"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          Your email
+          Name
         </label>
         <input
           type="email"
@@ -33,7 +33,7 @@ function Admin() {
           required
         />
       </div>
-      <div className="mb-6">
+      {/* <div className="mb-6">
         <label
           htmlFor="repeat-password"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -46,8 +46,8 @@ function Admin() {
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           required
         />
-      </div>
-      <div className="flex items-start mb-6">
+      </div> */}
+      {/* <div className="flex items-start mb-6">
         <div className="flex items-center h-5">
           <input
             id="terms"
@@ -69,7 +69,7 @@ function Admin() {
             terms and conditions
           </a>
         </label>
-      </div>
+      </div> */}
       <button
         type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
